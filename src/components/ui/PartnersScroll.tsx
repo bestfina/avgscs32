be1f425cfbeec471150ef508b05a5b81dd8a78dc
German Vl <tertydert@gmail.com>
2025-05-03 09:24:09 +0300

    parthers logo fix

--- src/components/ui/PartnersScroll.tsx
+++ src/components/ui/PartnersScroll.tsx
@@ -7,12 +7,13 @@ const PartnersScroll = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
   const technologies = [
     "https://cdn.tbank.ru/static/pfa-multimedia/images/e888870a-f15a-4684-968e-256ad5694e46.svg",
-    "https://meteorex.net/wp-content/uploads/2024/06/meteorex.svg",
+    "/assets/icons/meteorex.svg",
     "https://denissanko.com/wp-content/themes/Sanko/land-strategy/images/common/logo.svg",
-    "https://bisovet.pro/wp-content/uploads/2024/09/logo.svg",
+    "/assets/icons/bisovet.svg",
     "https://www.zolotarevka.ru/public/icons/logo-black.svg",
     "https://runabeton.ru/wp-content/themes/twentytwentyone/img/logo.svg",
     "/assets/icons/TAXIPRO.svg",
+    "/assets/images/parthers/klikmi.png",
     "https://bz-titan.ru/wp-content/themes/titan/img/logo.svg",
   ];
   useEffect(() => {
@@ -36,7 +37,7 @@ const PartnersScroll = () => {
     <div className="relative overflow-hidden w-full">
       <div ref={scrollRef} className="flex gap-md sm:gap-xs sm:w-36 sm:h-16" style={{ display: "inline-flex" }}>
         {[...technologies, ...technologies].map((tech, index) => (
-          <Image key={index} className="" alt="" src={tech} width={200} height={80}></Image>
+          <Image key={index} className="object-contain" alt="" src={tech} width={200} height={80}></Image>
         ))}
       </div>
     </div>
