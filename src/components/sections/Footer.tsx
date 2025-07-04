07b90456c31513c9b5400178de9c95c4bf2ef2b9
NurjahonErgashevMe <nurjahonergashevme@gmail.com>
2025-07-05 04:41:20 +0500

    added adaptive to footer for md

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from "next-intl";
 import Logo from "../ui/Logo";
 import { TransitionLink } from "@/lib/link";
 import SocialFooter from "../ui/SocialFooter";
-
 import FolderIcon from "public/assets/icons/folder.svg";
 import FirstSection from "../ui/FirstSection";
 import { FOOTER_PAGE_LINKS, SERVICE_SUB_MENU } from "@/constants";
@@ -31,20 +30,15 @@ const Footer = () => {
 
   return (
     <footer className="bg-[#0E202C] text-white" id="contact">
-      <div className="container ">
-        {/* Upper Section - Main Content */}
-        <div className="flex flex-wrap justify-between py-16 gap-8 lg:py-12 sm:py-8 sm:flex-col">
-          {/* Logo Column */}
-          <div className="flex-1 min-w-[200px] max-w-[300px] sm:max-w-none sm:mb-6">
+      <div className="container">
+        {/* Upper Section */}
+        <div className="flex flex-wrap justify-between py-16 gap-8 lg:py-12 md:py-10 md:flex-wrap md:gap-x-6 md:[&>div]:mb-4 sm:py-8 sm:flex-col">
+          <div className="flex-1 min-w-[200px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-6">
             <Logo big light />
           </div>
 
-          {/* Services Column */}
-          <div className="flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-6">
-            <h3
-              className="text-[20px] font-semibold text-white mb-4 leading-tight sm:text-[18px] sm:mb-4"
-              style={{ fontFamily: "var(--font-nunito)" }}
-            >
+          <div className="flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-6">
+            <h3 className="text-[20px] font-semibold text-white mb-4 leading-tight sm:text-[18px] md:text-end sm:text-start sm:mb-4" style={{ fontFamily: "var(--font-nunito)" }}>
               {t("main.footer.services")}
             </h3>
             <nav className="space-y-3 sm:space-y-2">
@@ -52,7 +46,7 @@ const Footer = () => {
                 <TransitionLink
                   key={item.id}
                   href={item.url}
-                  className="block text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed sm:text-[15px]"
+                  className="block text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed sm:text-[15px] md:text-end sm:text-start"
                 >
                   {t(item.title)}
                 </TransitionLink>
@@ -60,8 +54,7 @@ const Footer = () => {
             </nav>
           </div>
 
-          {/* Pages Column */}
-          <div className="flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-6">
+          <div className="flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-6">
             <nav className="space-y-3 sm:space-y-2">
               {FOOTER_PAGE_LINKS.map(link => (
                 <TransitionLink
@@ -75,8 +68,7 @@ const Footer = () => {
             </nav>
           </div>
 
-          {/* Contact Info Column */}
-          <div className="flex-1 min-w-[250px] max-w-[300px] sm:max-w-none">
+          <div className="flex-1 min-w-[250px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none">
             <div className="space-y-6 sm:space-y-4">
               <div className="text-end sm:text-start">
                 <a
@@ -107,7 +99,6 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Social Media - Mobile Only */}
         <div className="hidden sm:block py-2">
           <div className="w-full flex items-end justify-start gap-4 sm:mb-6">
             <SocialFooter />
@@ -115,24 +106,19 @@ const Footer = () => {
         </div>
 
         {/* Middle Section */}
-        <div className="flex flex-wrap justify-between py-8 gap-8 lg:py-8 sm:py-4 sm:flex-col">
-          {/* Company Info */}
-          <div className="flex-1 min-w-[200px] max-w-[300px] sm:max-w-none sm:mb-2">
+        <div className="flex flex-wrap justify-between py-8 gap-8 lg:py-8 md:py-6 md:flex-wrap md:gap-x-6 md:[&>div]:mb-4 sm:py-4 sm:flex-col">
+          <div className="flex-1 min-w-[200px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-2">
             <div className="space-y-2 sm:space-y-1">
               <p className="text-[16px] text-[#FFFFFF99] leading-relaxed sm:text-[14px]">
-                ИП Волков Александр <br className="sm:hidden"/> Александрович
+                ИП Волков Александр <br className="sm:hidden" /> Александрович
               </p>
               <p className="text-[16px] text-[#FFFFFF99] leading-relaxed sm:text-[14px]">ОГРН 000000000000</p>
               <p className="text-[16px] text-[#FFFFFF99] leading-relaxed sm:text-[14px]">ИНН 000000000000</p>
             </div>
           </div>
 
-          {/* Address */}
-          <div className="flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-6">
-            <h3
-              style={{ fontFamily: "var(--font-nunito)" }}
-              className="text-[18px] font-semibold text-white mb-1 leading-tight sm:text-[16px] sm:mb-2"
-            >
+          <div className="flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] md:text-end sm:text-start sm:max-w-none sm:mb-6">
+            <h3 className="text-[18px] font-semibold text-white mb-1 leading-tight sm:text-[16px] sm:mb-2" style={{ fontFamily: "var(--font-nunito)" }}>
               {t("main.footer.company_address")}
             </h3>
             <div className="space-y-1">
@@ -147,19 +133,17 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Download Presentation Button */}
-          <div className="flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-6 sm:flex sm:justify-center">
+          <div className="flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-6 sm:flex sm:justify-center">
             <button
               onClick={downloadPresentationWithFetch}
-              className="flex items-center justify-center gap-3 bg-transparent text-white text-[16px] hover:opacity-[0.7] transition-opacity duration-300 sm:text-[18px] bg-[#263641] px-[20px] rounded-lg h-[60px] w-full"
+              className="flex items-center justify-center gap-3 bg-transparent text-white text-[16px] hover:opacity-[0.7] transition-opacity duration-300 sm:text-[18px] bg-[#263641] px-[20px] rounded-lg h-[60px] md:h-[50px] md:text-[15px] sm:h-[60px] sm:text-[18px] w-full text-start"
             >
               <FolderIcon width={16} height={16} alt="Download" className="w-4 h-4 flex-shrink-0" />
               {t("main.footer.download_presentation")}
             </button>
           </div>
 
-          {/* Social Icons - Desktop Only */}
-          <div className="flex-1 min-w-[200px] max-w-[300px] sm:hidden">
+          <div className="flex-1 min-w-[200px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:hidden">
             <div className="flex items-center justify-end gap-4">
               <SocialFooter />
             </div>
@@ -167,10 +151,9 @@ const Footer = () => {
         </div>
 
         {/* Bottom Section */}
-        <div className="w-full flex flex-wrap justify-between py-16 gap-8 lg:py-12 sm:py-8 sm:flex-col border-t border-[#FFFFFF33] relative">
+        <div className="w-full flex flex-wrap justify-between py-16 gap-8 lg:py-12 md:py-8 md:flex-wrap md:gap-x-6 md:[&>div]:mb-4 sm:py-8 sm:flex-col border-t border-[#FFFFFF33] relative">
           <div className="w-full flex flex-wrap justify-between items-center gap-6 sm:flex-col sm:gap-4 sm:items-start">
-            {/* Copyright */}
-            <div className="order-1 sm:order-3 flex-1 min-w-[200px] max-w-[300px] sm:max-w-none sm:mb-3 sm:w-3/5">
+            <div className="order-1 sm:order-3 flex-1 min-w-[200px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-3 sm:w-3/5">
               <div className="text-[16px] text-[#FFFFFF99] leading-relaxed sm:text-[14px]">
                 <span className="text-white font-medium">CortexDigital © {new Date().getFullYear()}</span>
                 <br />
@@ -178,18 +161,16 @@ const Footer = () => {
               </div>
             </div>
 
-            {/* Privacy Policy */}
-            <div className="order-2 flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-3 sm:w-1/2">
+            <div className="order-2 flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-3 sm:w-1/2 md:text-end sm:text-start">
               <TransitionLink
                 href="/privacy"
-                className="text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed sm:text-[14px]"
+                className="text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed sm:text-[14px] "
               >
                 {t("main.footer.privacy_policy")}
               </TransitionLink>
             </div>
 
-            {/* Privacy Consent */}
-            <div className="order-3 sm:order-1 flex-1 min-w-[200px] max-w-[250px] sm:max-w-none sm:mb-3 sm:w-3/5">
+            <div className="order-3 sm:order-1 flex-1 min-w-[200px] max-w-[250px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:mb-3 sm:w-3/5">
               <TransitionLink
                 href="/"
                 className="text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed sm:text-[14px]"
@@ -198,8 +179,7 @@ const Footer = () => {
               </TransitionLink>
             </div>
 
-            {/* Back to Top */}
-            <div className="order-4 flex w-full justify-end min-w-[250px] max-w-[300px] sm:max-w-none sm:absolute sm:bottom-10 sm:right-[3px]">
+            <div className="order-4 flex w-full justify-end min-w-[250px] max-w-[300px] md:min-w-[45%] md:max-w-[45%] sm:max-w-none sm:absolute sm:bottom-10 sm:right-[3px]">
               <FirstSection
                 withIcon
                 className="text-[16px] text-[#FFFFFF99] hover:text-white transition-colors duration-300 leading-relaxed"
